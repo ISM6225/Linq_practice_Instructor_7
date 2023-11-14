e107b470f243b3d2d8320ae6da0989fc262763d3
ISM6225 <ISM6225Class@gmail.com>
2023-11-13 21:13:49 -0500

    Add project files.

---
+++ Linq_practice_Instructor_7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Linq_practice_Instructor_7.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
